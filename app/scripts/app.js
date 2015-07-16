a07b8705fa4779a5b901707c6e5f1d81bd8c7828
Spencer Davies <spencerhdavies@gmail.com>
2015-07-16 15:21:24 -0600

    started app.js

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+$(function() {
+	$('#newTaskForm').hide();
+	var listo = [];
+
+	function Task (task) {
+		this.task = task;
+		this.id = 'new';
+	}
+
+	var addTask = function(task) {
+		if(task) {
+			task = new Task(task);
+			listo.push(task);
+
+			$('#newItemInput').val('');
+			$('#newList').append('<a href="#" class="" id="item"><li class="list-group-item">' + task.task + '<span class="arrow pull-right"><i class="glyphicon glyphicon-arrow-right"></span></li></a>');
+		}
+	}
+})
